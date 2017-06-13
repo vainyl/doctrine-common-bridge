3305efd83f00906eefa8505b87edb99c962f3ab6
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-13 13:04:09 +0300

    Added DoctrineExtension

---
+++ src/Extension/DoctrineExtension.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Doctrine-common-bridge
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Doctrine\Common\Extension;
+
+use Vainyl\Core\Extension\AbstractExtension;
+
+/**
+ * Class DoctrineExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class DoctrineExtension extends AbstractExtension
+{
+}
