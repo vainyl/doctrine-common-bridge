df498a7142b3731dc3d671017138323151b648da
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-22 16:11:53 +0300

    Fixed alias definition

---
+++ src/AbstractDoctrineDomain.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Doctrine-common-bridge
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Doctrine\Common;
+
+use Doctrine\Common\Collections\Collection;
+use Vainyl\Core\ArrayInterface;
+
+/**
+ * Class AbstractDoctrineDomain
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+abstract class AbstractDoctrineDomain implements ArrayInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function toArray(): array
+    {
+        $array = [];
+        foreach (get_object_vars($this) as $field => $value) {
+            switch (true) {
+                case (false === is_object($value)):
+                    $array[$field] = $value;
+                    break;
+                case ($value instanceof ArrayInterface):
+                    $array[$field] = $value->toArray();
+                    break;
+                case ($value instanceof Collection):
+                    /**
+                     * @var ArrayInterface $item
+                     */
+                    foreach ($value->toArray() as $item) {
+                        $array[$field][] = $item->toArray();
+                    }
+            }
+        }
+
+        return $array;
+    }
+}
