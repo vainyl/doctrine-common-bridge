6d879e60833d96026b438fc52e0859172e4ed6bf
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-23 11:44:25 +0300

    Fixed alias definition

---
+++ src/Registry/DoctrineRegistry.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Doctrine-common-bridge
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Doctrine\Common\Registry;
+
+use Doctrine\Common\Persistence\AbstractManagerRegistry;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Class DoctrineRegistry
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class DoctrineRegistry extends AbstractManagerRegistry
+{
+    private $container;
+
+    /**
+     * DoctrineRegistry constructor.
+     *
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+        parent::__construct('registry', [], [], null, null, '');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getService($name)
+    {
+        return $this->container->get(sprintf('doctrine.%s.manager', $name));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function resetService($name)
+    {
+        return;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAliasNamespace($alias)
+    {
+        return '';
+    }
+}
